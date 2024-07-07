5aa65e24d1fb6a161b1d88669f2dd56c7dab74fc
TusharW4ni <reachtusharwani@gmail.com>
2024-07-06 20:15:51 -0500

    /search/courses api written

--- server/index.js
+++ server/index.js
@@ -39,18 +39,42 @@ function executeQuery(pool, query) {
 
 // --------------------------------------------------------------------------
 
-app.post("/search", async (req, res) => {
+app.post("/search/courses", async (req, res) => {
   console.log("req.body", req.body);
   const { searchTerm } = req.body;
   try {
     const results = await executeQuery(
       db,
-      `SELECT Course.id, Question.description, Question.tag, sd, d, n, a, sa, tot, med, mu, sig
-      FROM Course, Question, Evaluation
-      WHERE Course.id LIKE "%${searchTerm}%" AND Course.id=Evaluation.cid AND Question.id=Evaluation.qid
+      `SELECT Course.id, Question.description, Question.tag, Instructor.fName, Instructor.lName, sd, d, n, a, sa, tot, med, mu, sig
+      FROM Course, Question, Evaluation, Instructor
+      WHERE Course.id LIKE "%${searchTerm}%" AND Course.id=Evaluation.cid AND Question.id=Evaluation.qid AND Instructor.id=Course.instructId
       ORDER BY Course.id ASC;`
     );
-    res.send(results);
+    const transformedResults = {};
+    results.forEach((row) => {
+      if (!transformedResults[row.id]) {
+        transformedResults[row.id] = {
+          id: row.id,
+          questions: [],
+        };
+      }
+      transformedResults[row.id].questions.push({
+        description: row.description,
+        tag: row.tag,
+        fName: row.fName,
+        lName: row.lName,
+        sd: row.sd,
+        d: row.d,
+        n: row.n,
+        a: row.a,
+        sa: row.sa,
+        tot: row.tot,
+        med: row.med,
+        mu: row.mu,
+        sig: row.sig,
+      });
+    });
+    res.send(Object.values(transformedResults));
   } catch (error) {
     console.error("Error in POST /search", error);
     res.status(500).send("Internal Server Error");
